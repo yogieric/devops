fb3cd144dd296c17312898c3f25754bdc4be73c4
Eric (iCloud) Brown <ericbrown@khanacademy.com>
2021-02-01 08:47:36 -0800

    Add script

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = []
 
 setup(
     name="devopsutil",
-    version="0.0.2",
+    version="0.0.3",
     author="Eric Brown",
     author_email="yogieric@gmail.com",
     description="A collection of devops utilities",
@@ -15,6 +15,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yogieric/devops/",
     packages=find_packages(),
+    scripts=['scripts/hello.py']
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.9",
